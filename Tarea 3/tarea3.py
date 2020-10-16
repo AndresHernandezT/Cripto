42c61d8c0090307e6572283e282d08951c2cbe4e
AndresHernandezT <andres.hernandezt@mail.udp.cl>
2020-10-16 18:14:36 -0300

    Update tarea3.py

--- Tarea 3/tarea3.py
+++ Tarea 3/tarea3.py
@@ -2,18 +2,18 @@ from Crypto import Random
 from Crypto.Cipher import DES
 import base64
 
+#padding
 def pad(data):
     length = 16 - (len(data) % 16)
     data = data.decode() + chr(length)*length
     return data.encode()
 
-def unpad(data):
-    return data[:-ord(data[-1])]
-
+#encriptado
 def encrypt(message, passphrase, IV):
     des = DES.new(passphrase, DES.MODE_OFB, IV)
     return base64.b64encode(IV + des.encrypt(pad(message)))
 
+#Mensaje del usuario
 message = input("Ingrese un mensaje: ")
 message = message.encode()
 key = ""
@@ -26,6 +26,7 @@ while len(IV) != 8:
 IV = IV.encode()
 encrypted = encrypt(message, key, IV)
 
+#HTML
 string = """
 <p>Este sitio contiene un mensaje secreto</p>
 <div class="des-ofb" id='"""+encrypted.decode()+"""'></div>
